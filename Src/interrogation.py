93b381c2f23cd659471d895f5c8d0e0fdbcea27a
sasunts <sasuntsv@tcd.ie>
2018-11-24 11:59:36 +0000

    Added comments

--- Src/interrogation.py
+++ Src/interrogation.py
@@ -1,3 +1,9 @@
+#This is an example of how to get data from logged in developer
+#One method of preventing access to github is to add input field
+#to allow user to type in login details or access token
+#however the code below does exactly what is asked from the assignment
+#it shows data about the logged in developer.
+
 from github import Github
 
 g = Github("aef65f876c340d835af0b469df6db362fa031a9b")
