731907b80029585cb36576e5b2d37b450bf469de
sasunts <sasuntsv@tcd.ie>
2018-11-29 18:00:25 +0000

    visual changes, text alignment

--- Src/treemap.js
+++ Src/treemap.js
@@ -1,13 +1,13 @@
-var color = d3.scale.category10();
+var color = d3.scale.category20();
 
 var canvas = d3.select("body").append("svg")
   .attr("width",1470)
-  .attr("height",870);
+  .attr("height",800);
 
 d3.json("data.json",function (data){
 
     var treemap = d3.layout.treemap()
-      .size([1470,870])
+      .size([1470,800])
       .nodes(data)
 
     var cells = canvas.selectAll(".cells")
@@ -21,38 +21,42 @@ d3.json("data.json",function (data){
       .attr("y", function (d) {return d.y;})
       .attr("width", function (d) {return d.dx;})
       .attr("height", function (d) {return d.dy;})
-      .attr("fill", function (d) {return d.children ? null : color(d.parent.name); })
+      .attr("fill", "#70D7E3")
       .attr("stroke", "#fff")
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut)
 
     cells.append("text")
-      .attr("x", function (d){return d.x + d.dx /2})
+      .attr("x", function (d){return ((d.x + d.dx /2)-40)})
       .attr("y", function (d){return d.y + d.dy /2})
-      .attr("text-acnhor", "middle")
+      .attr("text-acnhor", "left")
       .text(function (d) {return d.children ? null : d.name;})
 
+        var k = 0;
 
       function handleMouseOver(d, i) {
           d3.select(this).attr({
             fill: "orange"
           });
 
+
           cells.append("text").attr({
-              id: "t" + d.x + "-" + d.y + "-" + i,
-              x: function() { return d.x + d.dx /3 },
+              id: k,
+              x: function() { return ((d.x + d.dx /3)-40) },
               y: function() { return d.y + d.dy /3 }
           })
           .text(function() {
             return "Total Commits: " +d.value;
           });
+          k++;
         }
 
     function handleMouseOut(d, i) {
           // Use D3 to select element, change color back to normal
           d3.select(this).attr({
-            fill: color(d.parent.name),
+            fill: "#70D7E3",
           });
-          d3.select("t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
+          d3.select(k).remove();  // Remove text location
+          k--;
         }
   })
